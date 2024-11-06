58b4e7c415100781e144fa57315567b6fb9d07f3
Faareoh <m.raoux34@gmail.com>
2024-11-06 12:14:48 +0100

    Add possibility to check multiple servers instead of only KS-A

--- index.ts
+++ index.ts
@@ -24,6 +24,39 @@ enum DatacenterLocalization {
   waw = 'Pologne (Varsovie - WAW)',
 }
 
+class Server {
+  public readonly NAME: string;
+
+  public readonly SERVER_CODE: string;
+
+  public readonly WEB_CODE: string;
+
+  private readonly API_BASE_URL = 'https://www.ovh.com/engine/apiv6/dedicated/server/datacenter/availabilities/?excludeDatacenters=false';
+
+  private readonly WEB_BASE_URL = 'https://eco.ovhcloud.com/fr/kimsufi';
+
+  constructor(NAME: string, SERVER_CODE: string, WEB_CODE: string) {
+    this.NAME = NAME;
+    this.SERVER_CODE = SERVER_CODE;
+    this.WEB_CODE = WEB_CODE;
+  }
+
+  public getApiURL(): string {
+    return `${this.API_BASE_URL}&planCode=${this.SERVER_CODE}&server${this.SERVER_CODE}`;
+  }
+
+  public getWebURL(): string {
+    return `${this.WEB_BASE_URL}/${this.WEB_CODE}/`;
+  }
+}
+
+const SERVERS: Server[] = [
+  new Server('KS-A', '24ska01', 'ks-a'),
+  new Server('KS-LE-B', '25skleb01', 'ks-le-b')
+];
+
+
+
 class OvhKSAWatcher extends WebhookClient {
   private alreadyKnownLocations: string[] = []
 
@@ -36,13 +69,13 @@ class OvhKSAWatcher extends WebhookClient {
     super({ url: Bun.env.DISCORD_WEBHOOK_URL });
   }
 
-  async sendToWebhook(res: OvhResponse, dc: OVHDatacenter): Promise<void> {
+  async sendToWebhook(server: Server, res: OvhResponse, dc: OVHDatacenter): Promise<void> {
     const embed = new EmbedBuilder()
-      .setTitle(dc.availability === 'unavailable' ? 'Serveur KS-A indisponible' : 'Nouveau serveur KS-A disponible')
+      .setTitle(dc.availability === 'unavailable' ? `Serveur ${server.NAME} indisponible` : `Nouveau serveur ${server.NAME} disponible:`)
       .setDescription(dc.availability === 'unavailable' ?
-        `Le serveur KS-A en ${bold(DatacenterLocalization[dc.datacenter] || dc.datacenter)} est de nouveau indisponible` :
-        `Un nouveau serveur KS-A est disponible en ${bold(DatacenterLocalization[dc.datacenter] || dc.datacenter)}`)
-      .setURL('https://eco.ovhcloud.com/fr/kimsufi/ks-a/')
+        `Le serveur ${server.NAME} en ${bold(DatacenterLocalization[dc.datacenter] || dc.datacenter)} est de nouveau indisponible` :
+        `Un nouveau serveur ${server.NAME} est disponible en ${bold(DatacenterLocalization[dc.datacenter] || dc.datacenter)}`)
+      .setURL(server.getWebURL())
       .addFields({
         name: 'Serveur',
         value: res.server,
@@ -59,49 +92,49 @@ class OvhKSAWatcher extends WebhookClient {
       .setColor(dc.availability === 'unavailable' ? 'Red' : 'Green')
       .setTimestamp();
 
-    console.log(embed.toJSON());
-
     await this.send({
       embeds: [embed],
-      username: 'OVH KS-A Watcher',
+      username: `OVH ${server.NAME} Watcher`,
       ...(Bun.env.DISCORD_OWNER_ID ? { content: userMention(Bun.env.DISCORD_OWNER_ID) } : {})
     });
   }
 
   async watch(): Promise<void> {
-    const res = await fetch('https://www.ovh.com/engine/apiv6/dedicated/server/datacenter/availabilities/?excludeDatacenters=false&planCode=24ska01&server=24ska01', {
-      method: 'GET',
-    });
+    Object.values(SERVERS).forEach(async (server) => {
+      const res = await fetch(server.getApiURL(), {
+        method: 'GET',
+      });
 
-    const data = await res.json() as OvhResponse[];
+      const data = await res.json() as OvhResponse[];
 
-    data.forEach(async (item) => {
-      item.datacenters.forEach(async (dc) => {
-        if (dc.availability === 'unavailable' && this.alreadyKnownLocations.includes(dc.datacenter)) {
-          await this.sendToWebhook(item, dc);
-          this.alreadyKnownLocations = this.alreadyKnownLocations.filter((loc) => loc !== dc.datacenter);
-          console.log(this.alreadyKnownLocations);
-          return;
-        }
+      data.forEach(async (item) => {
+        item.datacenters.forEach(async (dc) => {
+          if (dc.availability === 'unavailable' && this.alreadyKnownLocations.includes(dc.datacenter)) {
+            await this.sendToWebhook(server, item, dc);
+            this.alreadyKnownLocations = this.alreadyKnownLocations.filter((loc) => loc !== dc.datacenter);
+            console.log(this.alreadyKnownLocations);
+            return;
+          }
 
-        if (dc.availability === 'unavailable') {
-          return;
-        }
+          if (dc.availability === 'unavailable') {
+            return;
+          }
 
-        if (this.alreadyKnownLocations.includes(dc.datacenter)) {
-          return;
-        }
+          if (this.alreadyKnownLocations.includes(dc.datacenter)) {
+            return;
+          }
 
-        await this.sendToWebhook(item, dc);
-        this.alreadyKnownLocations.push(dc.datacenter);
+          await this.sendToWebhook(server, item, dc);
+          this.alreadyKnownLocations.push(dc.datacenter);
+        });
       });
     });
   }
 
   async start(): Promise<void> {
-    await this.send({ content: 'Lancement du watcher KS-A', username: 'OVH KS-A Watcher' });
+    await this.send({ content: 'Lancement du watcher KS', username: 'OVH KS Watcher' });
     await this.watch();
-    setInterval(() => this.watch(), 60000); // 60 seconds by default
+    setInterval(() => this.watch(), 30000);
   }
 }
 
